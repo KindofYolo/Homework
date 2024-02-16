bfa0ac9d2be73eeb6dc29719fd8a504ab6e2bbd6
KindofYolo <67740841+KindofYolo@users.noreply.github.com>
2024-02-16 21:18:32 +0200

    Add files via upload

--- HW3_1.js
+++ HW3_1.js
@@ -1,36 +1,27 @@
-  /* function drawTriangle(size, symbol) {
-    for (let x = 1; x <= size; x++) {
-        let line = '';
-        for (let y = 0; y < x; y++) {
-            line += symbol;
-        }
+function drawTriangle(height, symbol) {
+    let line = '';
+
+    for (let i = 1; i <= height; i++) {
+        line += symbol;
         console.log(line);
-    }
- }
-
-drawTriangle(10, "A");   */
-
-
-
-
-
-
-
-function drawTriangle(size, symbol) {
-    let x = 1;
-
-    while (x <= size) {
-        let y = 0;
-        let line = '';
-
-        while (y < x) {
-            line += symbol;
-            y++;
-        }
-
-        console.log(line);
-        x++;
     }
 }
 
 drawTriangle(8, '$');
+
+
+
+
+
+/*function drawTriangle(height, symbol) {
+    let line = '';
+    let i = 1;
+
+    while (i <= height) {
+        line += symbol;
+        console.log(line);
+        i++;
+    }
+}
+
+drawTriangle(10, '=');    */
--- HW3_2.js
+++ HW3_2.js
@@ -1,7 +1,11 @@
 let sum = 0;
 
-for (let x = 1; x<=100;x++ )
-if (x %3 != 0){
-    sum=x+sum
+for (let x = 1; x <= 100; x++) {
+    if (x % 3 === 0) {
+        continue; 
+    }
+    
+    sum += x;
 }
+
 console.log(sum);
